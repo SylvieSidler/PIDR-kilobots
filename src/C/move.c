9d85b8adff148875e0ee3181f66b5d745d0a0799
SIDLER Sylvie <sylvie.sidler@telecomnancy.eu>
2025-03-20 17:26:59 +0100

    move prêt pour test ?

--- src/C/move.c
+++ src/C/move.c
@@ -1,17 +1,64 @@
 #include <kilolib.h>
 
+////////////////////////////////////////////////////////////////////////
+// SETUP
+
 // declare constants
 #define FORWARD_GREEN 0
 #define LEFT_RED 1
 #define RIGHT_BLUE  2
 
+// declare motion variable type
+typedef enum {
+    STOP,
+    FORWARD,
+    LEFT,
+    RIGHT
+} motion_t;
+
 // declare variables
-uint8_t state = FORWARD_GREEN;
-uint8_t state_needs_update = 1;
-uint8_t color_needs_update = 0;
-uint32_t last_update;
+typedef struct {
+    uint32_t last_update;
+    int N_Neighbors; 
+    uint8_t state;
+    uint8_t state_needs_update ;
+    uint8_t color_needs_update;
+} USERDATA;
+
+extern USERDATA *mydata;
+
+extern char* (*callback_botinfo) (void);
+char *botinfo(void);
+
+
+#ifdef SIMULATOR /////////
+
+#include <stdio.h>    // for printf
+int UserdataSize = sizeof(USERDATA);
+USERDATA *mydata;
+
+static char botinfo_buffer[10000];
+char *botinfo(void) {
+    int n;
+    char *p = botinfo_buffer;
+    n = sprintf (p, "ID: %d ", kilo_uid);
+    //p += n;
+    //n = sprintf (p, "Ns: %d, dist: %d\n ", mydata->N_Neighbors, find_nearest_N_dist());
+    p += n;
+
+    return botinfo_buffer;
+}
+
+#endif ///////////////////
+
+
+////////////////////////////////////////////////////////////////////////
+// CODE 
 
 void setup() {
+    mydata->state = FORWARD_GREEN;
+    mydata->state_needs_update = 1;
+    mydata->color_needs_update = 0;
     spinup_motors();
     set_motors(kilo_straight_left, kilo_straight_right);
     delay(2000);
@@ -25,25 +72,25 @@ void setup() {
     delay(2000);
     set_motors(0,0);
     
-    last_update = kilo_ticks; // init timer
+    mydata->last_update = kilo_ticks; // init timer
 }
 
 void loop() {
     // Change state every 64 clock ticks (roughly 2 seconds)
-    if (kilo_ticks > last_update + 64) {
-        last_update = kilo_ticks;
-        state = (state + 1) % 3;
-        state_needs_update = 1;
-        color_needs_update = 0;
+    if (kilo_ticks > mydata->last_update + 64) {
+        mydata->last_update = kilo_ticks;
+        mydata->state = (mydata->state + 1) % 3;
+        mydata->state_needs_update = 1;
+        mydata->color_needs_update = 0;
     }
 
-    if (kilo_ticks > last_update + 32 && !color_needs_update) {
+    if (kilo_ticks > mydata->last_update + 32 && !mydata->color_needs_update) {
         set_color(RGB(1,1,1));
-        color_needs_update = 1;
+        mydata->color_needs_update = 1;
     }
 
-    if (state_needs_update) {
-        switch(state) {
+    if (mydata->state_needs_update) {
+        switch(mydata->state) {
             case FORWARD_GREEN:
                 set_color(RGB(0,1,0));
                 spinup_motors();
@@ -60,7 +107,7 @@ void loop() {
                 set_motors(0, kilo_turn_right);
                 break;
         }
-        state_needs_update = 0;
+        mydata->state_needs_update = 0;
     }
 }
 
