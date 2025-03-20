b36f5f0569b8edcc2d1a35aaccde13d27c4a64e1
Zoe Vernicos <zoe.vernicos@telecomnancy.eu>
2025-03-20 17:29:08 +0100

    modifs move_simple.c

--- src/C/move_simple.c
+++ src/C/move_simple.c
@@ -1,4 +1,51 @@
-#include <kilolib.h>
+#include <kilombo.h>
+#include <math.h>
+
+////////////////////////////////////////////////////////////////////////
+// SETUP
+
+// declare motion variable type
+typedef enum {
+    STOP,
+    FORWARD,
+    LEFT,
+    RIGHT
+} motion_t;
+
+// declare variables
+typedef struct {
+    uint32_t last_update;
+    int N_Neighbors; 
+} USERDATA;
+
+extern USERDATA *mydata;
+
+extern char* (*callback_botinfo) (void);
+char *botinfo(void);
+
+#ifdef SIMULATOR /////////
+
+#include <stdio.h>    // for printf
+int UserdataSize = sizeof(USERDATA);
+USERDATA *mydata;
+
+static char botinfo_buffer[10000];
+char *botinfo(void) {
+    int n;
+    char *p = botinfo_buffer;
+    n = sprintf (p, "ID: %d ", kilo_uid);
+    //p += n;
+    //n = sprintf (p, "Ns: %d, dist: %d\n ", mydata->N_Neighbors, find_nearest_N_dist());
+    p += n;
+
+    return botinfo_buffer;
+}
+
+#endif ///////////////////
+
+
+////////////////////////////////////////////////////////////////////////
+// CODE 
 
 void setup() {     
     // spinup motors
@@ -27,6 +74,10 @@ void loop() {
 
 int main() {
     kilo_init();
+
+    SET_CALLBACK(botinfo, botinfo);
+    SET_CALLBACK(reset, setup);
+
     kilo_start(setup, loop);
 
     return 0;
