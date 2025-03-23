04deab9b2f4fd2fdaa203ef96d876394905351e8
martinbiche67@gmail.com <martin.biche@telecomnancy.eu>
2025-03-23 17:03:51 +0100

    Ajout du choix de la résolution et de la caméra à l'aide d'une commande GET vers le serveur

--- src/main.py
+++ src/main.py
@@ -3,18 +3,23 @@ import numpy as np
 import requests
 from functionsImageDetection import *
 import json
-
-
+import pickle
 
 def getImage():
     r = requests.get("http://localhost:5001/start")
-    r= requests.get("http://localhost:5001/capture_image")
+    r = requests.get("http://localhost:5001/capture_image")
     decodedjson = r.json()
     deserializedjson = pickle.loads(json.loads(decodedjson).encode('latin-1'))
     return deserializedjson
-    #cv2.imshow("Deserialized Image", deserializedjson)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+    
+    cv2.imshow("image", getImage())
+
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
 
 def detectCircles():
     img = getImage()
@@ -26,6 +31,6 @@ def showCircles():
     show_circles_on_img(coordandimg[0],coordandimg[1])
 
 if __name__=="__main__":
-    cv2.imshow("imqge",getImage())
+    cv2.imshow("image",getImage())
     cv2.waitKey(0)
     showCircles()
