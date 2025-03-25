74c8311398e8a6ac36a3f58c18cdaa80ea6ddbbf
SIDLER Sylvie <sylvie.sidler@telecomnancy.eu>
2025-03-25 21:49:40 +0100

    fix du setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,22 @@
-if [ -e .venv ]
+if [ -e .venv ] || [ -e venv ]
 then
-    echo ".venv already setup"
+    if [ -e .venv ] 
+    then
+        echo ".venv already setup"
+        echo '.venv/'>>.gitignore
+        source .venv/bin/activate
+    else 
+        echo "venv already setup"
+        echo 'venv/'>>.gitignore
+        source venv/bin/activate
+    fi
 else
     echo "Setting up .venv and installing requirements"
     python3 -m venv .venv
     pip install --upgrade pip
     pip install -r requirements.txt
 fi
-echo '.venv/'>>.gitignore
 echo '.vscode/'>>.gitignore
 echo '__pycache__/'>>.gitignore
 echo '.DS_Store'>>.gitignore
-source .venv/bin/activate
 echo "Setup complete"
