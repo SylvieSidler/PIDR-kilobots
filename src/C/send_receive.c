031f0fcdf1d906c76a792b3c632884e976cb4f80
Zoe Vernicos <zoe.vernicos@telecomnancy.eu>
2025-04-02 15:15:03 +0200

    modified send_receive and added role assignement in the setup

--- src/C/send_receive.c
+++ src/C/send_receive.c
@@ -1,9 +1,9 @@
 #include <kilombo.h>
 
 uint8_t new_message = 0;
-#define num_bots 10
+#define num_bots 2
 #define com_radius 5
-
+uint16_t dist;
 ////////////////////////////////////////////////////////////////////////
 // SETUP
 
@@ -61,15 +61,7 @@ void message_tx_success() {
 }
 
 void message_rx(message_t *m, distance_measurement_t *d) {
-    uint16_t distance = (d->high_gain << 8) | d->low_gain;
-    if (distance < com_radius) {
-        uint8_t neighbor_id = m->data[0];  // Get the bot ID from the received message
-        if (mydata->communicated_with[neighbor_id] == 0) {  // If we haven't communicated with this bot yet
-            mydata->communicated_with[neighbor_id] = 1;  // Mark as communicated with
-            new_message = 1;  // Flag that a new message has been received
-            printf("Message received from bot %d at distance %d\n", neighbor_id, distance);  // Debugging line
-        }
-    }
+    
 }
 
 void setup() {
@@ -89,6 +81,8 @@ void loop() { ////////////////// LOOP
         set_color(RGB(1,1,0));
         mydata->last_update = 0;
     }
+
+    ///robots pairs vs robots impairs ecouteur envoyeurs
     if (new_message) {
         new_message = 0;
         set_color(RGB(1,0,0));
@@ -100,16 +94,18 @@ void loop() { ////////////////// LOOP
     }
     for (int i = 0; i <num_bots; i++) {
         if (mydata->communicated_with[i] == 0) {  // Check if this neighbor has already communicated
-            // Send message only if the bot is within communication radius
-            mydata->transmit_msg.data[0] = kilo_uid;  // Include our bot ID in the message
-            mydata->transmit_msg.crc = message_crc(&mydata->transmit_msg);  // Update the CRC
-            kilo_message_tx();  // Send the message
-            mydata->message_sent = 1;  // Mark message as sent
-            mydata->last_update = kilo_ticks;  // Reset timer
-            printf("Message sent to bot %d\n", i);
+            if (dist < com_radius) {// Send message only if the bot is within communication radius
+                mydata->transmit_msg.data[0] = kilo_uid;  // Include our bot ID in the message
+                mydata->transmit_msg.crc = message_crc(&mydata->transmit_msg);  // Update the CRC
+                kilo_message_tx();  // Send the message
+                mydata->message_sent = 1;  // Mark message as sent
+                mydata->last_update = kilo_ticks;  // Reset timer
+                printf("Message sent to bot %d\n", i);
+             }
+            }
         }
     }
-}
+
 
 
 int main() {
