815dc7dc5e92c82b3c2fbde8c3bbefd12a9b7ea3
SIDLER Sylvie <sylvie.sidler@telecomnancy.eu>
2025-04-26 18:14:59 +0200

    namingGame : ajout du setup et main

--- src/C/namingGameFunctions.c
+++ src/C/namingGameFunctions.c
@@ -6,7 +6,7 @@ void generateWord(void) {
     mydata->personalWord = globalWordList[randn];
 }
 
-// create a link between the object and the word
+// create a link between the object and the word in `mydata->links`
 void generateLink(uint8_t object, uint8_t word) {
     link_obj_word link;
     link.object = object;
@@ -18,8 +18,34 @@ void generateLink(uint8_t object, uint8_t word) {
 // deletes all links to the object except the word in parameters 
 void deleteLinksExceptWord(uint8_t object, uint8_t keepWord) {
     for (uint8_t i = 0; i < N; i++) {
-        if (mydata->links[i].word != keepWord) { // && mydata->links[i].object == object
+        if (mydata->links[i].word != keepWord) { // && mydata->links[i].object == object // si plusieurs objets
             mydata->links[i].boolean = false;
         }
     }
+}
+
+
+void setup() { 
+    mydata->obj = OBJECT;
+    mydata->stateLS = LISTENER;
+    generateWord();
+    printf("setup object: %d, word: %d\n", mydata->obj, mydata->personalWord);
+    generateLink(mydata->obj, mydata->personalWord);
+    set_color(colors[0]);
+}
+
+
+int main()
+{
+    // Initialize the kilobots 
+    kilo_init();
+
+    // Set the callback functions
+    kilo_message_tx=message_tx;
+    kilo_message_tx_success = message_tx_success;
+    kilo_message_rx=message_rx;
+    
+    kilo_start(setup, loop);
+    
+    return 0;
 }
