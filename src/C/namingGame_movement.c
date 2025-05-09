19e96d4c922d603ce9d48ec88e113821423c07ae
Zoe Vernicos <zoe.vernicos@telecomnancy.eu>
2025-05-09 13:31:11 +0200

    separation des suex namingGames

--- src/C/namingGame_movement.c
+++ src/C/namingGame_movement.c
@@ -21,6 +21,9 @@ typedef struct {
     uint32_t receive_cpt;
     uint32_t state_cpt;
     uint32_t delay_start;
+    enum direction dir;
+    uint32_t move_cpt;
+    uint32_t last_message_received_time;
 } USERDATA;
 
 
@@ -52,6 +55,47 @@ void deleteLinksExceptWord(uint8_t object, uint8_t keepWord) {
     }
 }
 
+void random_direction(){
+    int randn= rand_soft()%4;
+    mydata->dir = randn;
+}
+
+void move(){
+    switch(mydata->dir){
+        case RIGHT:
+            spinup_motors();
+            set_motors(0,kilo_straight_right);
+            break;
+        case LEFT:
+            spinup_motors();
+            set_motors(kilo_straight_left,0);
+            break;
+        case FORWARD:
+            spinup_motors();
+            set_motors(kilo_straight_left,kilo_straight_right);
+            break;
+        case BACKWARD:
+            spinup_motors();
+            set_motors(kilo_turn_left,0);
+            break;
+        case STOP:
+            set_motors(0,0);
+            break;
+    }
+}
+
+void move_kilobot(){
+    uint8_t rand = rand_soft()%100;
+    if (rand> 50){
+        random_direction();
+        move();
+        mydata->move_cpt = kilo_ticks;
+    }
+    else{
+        move();
+        mydata->move_cpt = kilo_ticks;
+    }
+}
 
 
 message_t *message_tx(){
@@ -82,19 +126,21 @@ void setup() {
     //printf("setup object: %d, word: %d\n", mydata->object, mydata->personalWord);
     generateLink(mydata->object, mydata->personalWord);
     //mydata->last_update = kilo_ticks;
+    //set_color(colours[0]);
     mydata->send_cpt=kilo_ticks;
     mydata->receive_cpt=kilo_ticks;
     mydata->state_cpt=kilo_ticks;
     mydata->delay_start=0;//rand()%320;
+    mydata->move_cpt = kilo_ticks;
+    mydata->last_message_received_time= kilo_ticks;
+    mydata->dir = STOP;
     rand_seed(rand_hard());
     generateWord();
-    set_color(colours[0]);
 }
 
 
 void loop() {
-
-    if (mydata->delay_start--<0){
+    if (mydata->delay_start--<=0){
         return;
     }
     set_color(colours[mydata->personalWord]);
@@ -102,7 +148,6 @@ void loop() {
         case SPEAKER:
             //set_color(RGB(3,0,0));
             if (kilo_ticks > mydata->send_cpt + SEND_DELAY){
-               
                 mydata->send_cpt =kilo_ticks;
                 mydata->transmit_msg.type= NORMAL;
                 mydata->transmit_msg.data[0]=mydata->object;
@@ -111,13 +156,16 @@ void loop() {
                 mydata->transmit_msg.crc = message_crc(&mydata->transmit_msg);
                 mydata->message_ready = 1;
             }
+            move();
             break;
         case LISTENER:
             //set_color(RGB(0,0,3));
             if (kilo_ticks > mydata->receive_cpt+ RECEIVE_DELAY){
-               
                 mydata->receive_cpt=kilo_ticks;
                 if (mydata->new_message==1){
+                    mydata->last_message_received_time=kilo_ticks;
+                    mydata->dir=STOP;
+                    move();
                     int random = rand_soft()%10;
                     if (random > 6){
                         mydata->personalWord = mydata->rvd_message.data[1];
@@ -127,22 +175,38 @@ void loop() {
                     mydata->new_message=0;
                 }
             }
+            if (kilo_ticks>mydata->last_message_received_time+NO_RECEPTION){
+                mydata->last_message_received_time=kilo_ticks;
+                move_kilobot();
+                if (kilo_ticks> mydata->move_cpt+MOVE_DELAY){
+                    set_motors(0,0);
+                    mydata->dir = STOP;
+                    mydata->move_cpt = kilo_ticks;
+                   
+                }
+            }
+
             break;
     }
     if (kilo_ticks> mydata->state_cpt +STATE_DELAY){
         mydata->state_cpt=kilo_ticks;
         uint8_t random = rand_soft()%100;
-        if (random >80){
+        if (random >50){
             if (mydata->stateLS ==SPEAKER) {
                 mydata->stateLS = LISTENER;
                 mydata->message_ready=0;
                 mydata->receive_cpt=kilo_ticks;
+                mydata->move_cpt = kilo_ticks;
+                mydata->last_message_received_time=kilo_ticks;
                 //set_color(RGB(0,0,3));
             }
             else{
                 mydata->stateLS = SPEAKER;
                 mydata->send_cpt=kilo_ticks;
                 //set_color(RGB(3,0,0));
+                mydata->dir=STOP;
+                mydata->move_cpt = kilo_ticks;
+                move();
             }
             
         }
@@ -165,3 +229,4 @@ int main()
     
     return 0;
 }
+
